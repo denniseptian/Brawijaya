cb2cef48d83b7aa49a742ad9584126d03a63dd5d
qe <dennix.apk@gmail.com>
2016-06-04 10:29:21 +0700

    New veature (ev rusak)

---
+++ application/config/recaptcha.php
@@ -0,0 +1,13 @@
+<?php
+
+// To use reCAPTCHA, you need to sign up for an API key pair for your site.
+// link: http://www.google.com/recaptcha/admin
+$config['recaptcha_site_key'] = '6LfnuyETAAAAAJgMmsBecO32FNpv4MnbRMiLKUBm';
+$config['recaptcha_secret_key'] = '6LfnuyETAAAAACdTAmOw3eA4Ymqe5wtmfIW3LuLF';
+
+// reCAPTCHA supported 40+ languages listed here:
+// https://developers.google.com/recaptcha/docs/language
+$config['recaptcha_lang'] = 'en';
+
+/* End of file recaptcha.php */
+/* Location: ./application/config/recaptcha.php */
